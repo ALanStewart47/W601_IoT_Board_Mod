2feb291c9a6baa10495689fba69c5f65a26b638a
ALanStewart47 <843530433@qq.com>
2023-05-18 13:09:52 +0800

    1、完成实验1

--- examples/01_basic_led_blink/applications/main.c
+++ examples/01_basic_led_blink/applications/main.c
@@ -6,6 +6,7 @@
  * Change Logs:
  * Date           Author       Notes
  * 2019-05-07     yangjie      first implementation
+ * 2023-05-18			ALanStewart  add Parity judgment
  */
 
 #include <rtthread.h>
@@ -19,16 +20,30 @@
 /* 配置 LED 灯引脚 */
 #define LED_PIN     PIN_LED_R
 
+/* 判断奇偶 */
+int odd_even(uint16_t i)
+{
+	if(i%2 == 0)
+		return 1;
+	else	
+		return 0;
+}
+
+
 int main(void)
 {
     unsigned int count = 1;
+		
 
     /* 设置 LED 引脚为输出模式 */
     rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
+		
 
     while (count > 0)
     {
         /* LED 灯亮 */
+			if(odd_even(count))
+			{
         rt_pin_write(LED_PIN, PIN_LOW);
         LOG_D("led on, count: %d", count);
         rt_thread_mdelay(500);
@@ -38,7 +53,9 @@ int main(void)
         LOG_D("led off");
         rt_thread_mdelay(500);
 
-        count++;
+        //count++;
+			}
+			count++;
     }
 
     return 0;
